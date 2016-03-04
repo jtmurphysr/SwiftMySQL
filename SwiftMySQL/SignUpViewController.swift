64b6714c4240b84293cf0cce6292d8bfaa6abaff
John Murphy <john@bicycle.ninja>
2016-03-04 09:34:21 +0100

    SignUp

---
+++ SwiftMySQL/SignUpViewController.swift
@@ -0,0 +1,179 @@
+//
+//  SignUpViewController.swift
+//  SwiftMySQL
+//
+//  Created by John Murphy on 3/3/16.
+//  Copyright © 2016 John Murphy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var profilePhotoImageView: UIImageView!
+    @IBOutlet weak var userEmailAddressTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var userPasswordConfirmTextField: UITextField!
+    @IBOutlet weak var userFirstNameTextField: UITextField!
+    @IBOutlet weak var userLastNameTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func selectProfilePhotoButtonTapped(sender: AnyObject) {
+        
+        let myPickerController = UIImagePickerController()
+        myPickerController.delegate = self
+        myPickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        
+        self.presentViewController(myPickerController, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func signUpButtonTapped(sender: AnyObject) {
+        
+        
+        //dismiss keyboard
+        
+        self.view.endEditing(true)
+        
+        let userEmail = userEmailAddressTextField.text
+        let userPassword = userPasswordTextField.text
+        let userPassConfirm = userPasswordConfirmTextField.text
+        let userFirstName = userFirstNameTextField.text
+        let userLastName = userLastNameTextField.text
+        
+        
+        if (userEmail!.isEmpty || userPassword!.isEmpty || userPassConfirm!.isEmpty || userFirstName!.isEmpty || userLastName!.isEmpty) {
+            
+            let myAlert = UIAlertController(title: "Error", message: "All Fields Are Required", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            
+            myAlert.addAction(okAction)
+            
+            self.presentViewController(myAlert, animated: true, completion: nil)
+            
+            return
+        }
+        
+        
+        if (userPassword != userPassConfirm) {
+            
+            
+            let myAlert = UIAlertController(title: "Error", message: "Passwords Don't Match", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            
+            myAlert.addAction(okAction)
+            
+            self.presentViewController(myAlert, animated: true, completion: nil)
+            
+            return
+        }
+        
+        
+        
+        //send HTTP POST
+        let myUrl = NSURL(string: "http://localhost/SwiftApp/scripts/registerUser.php");
+        let request = NSMutableURLRequest(URL:myUrl!);
+        request.HTTPMethod = "POST";
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        let postString = "userEmail=\(userEmail!)&userFirstName=\(userFirstName!)&userLastName=\(userLastName!)&userPassword=\(userPassword!)";
+        
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+        
+        print(request)
+        print(request.HTTPBody)
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: { (data:NSData?, response:NSURLResponse?, error:NSError?) -> Void in
+            
+            dispatch_async(dispatch_get_main_queue())
+                {
+                    
+                    if error != nil {
+                        self.displayMessage(error!.localizedDescription)
+                        return
+                    }
+                    
+                    do {
+                        let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+                        
+                        if let parseJSON = json {
+                            
+                            let userId = parseJSON["userId"] as? String
+                            
+                            if( userId != nil) {
+                                let myAlert = UIAlertController(title: "Alert", message: "Registration successful", preferredStyle: UIAlertControllerStyle.Alert);
+                                
+                                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default){(action) in
+                                    
+                                    self.dismissViewControllerAnimated(true, completion: nil)
+                                    
+                                }
+                                myAlert.addAction(okAction);
+                                self.presentViewController(myAlert, animated: true, completion: nil)
+                            } else {
+                                
+                                let errorMessage = parseJSON["message"] as? String
+                                if(errorMessage != nil)
+                                {
+                                    self.displayMessage(errorMessage!)
+                                }
+                            }
+                            
+                        }
+                        
+                        
+                    } catch {
+                        print(error)
+                    }
+                    
+                    
+            }
+        })
+        task.resume()
+        
+        
+        
+        
+        //let userProfileImageData = UIImageJPEGRepresentation(profilePhotoImageView.image!, 1)
+    }
+    
+    
+    
+    func displayMessage(userMessage:String) {
+        
+        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        
+        myAlert.addAction(okAction)
+        
+        self.presentViewController(myAlert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    @IBAction func cancelButtonTapped(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    
+}
--- SwiftMySQL/ViewController.swift
+++ SwiftMySQL/ViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ 
 
 
 }
